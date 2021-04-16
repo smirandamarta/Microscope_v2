ef608440053b84752f3fa8785862de9a36c21c55
JanGoeran <71365036+JanGoeran@users.noreply.github.com>
2021-04-16 11:12:50 +1000

    a few fixes and MSM08 analysis

---
+++ MSM08.py
@@ -0,0 +1,92 @@
+from eval import *
+
+
+S = ResultsDF('sample_A')
+print(S.df.head())
+
+#drop dead devices and cut off beginning
+S.df = S.df.loc[(S.df.time > 500)]
+liveList = S.get_live_devices(cutoff=1e-5)
+S.df = S.df.loc[(S.df.device.isin(liveList))]
+
+#split into 3 data frames
+df1 = S.df.loc[(S.df.time > 500) & (S.df.repeat < 45) & (S.df.device.isin(liveList))]
+df2 = S.df.loc[(S.df.repeat > 45) & (S.df.repeat < 89) & (S.df.device.isin(liveList))]
+df3 = S.df.loc[(S.df.repeat > 90) & (S.df.device.isin(liveList))]
+
+#Get G average for each device
+av1 = get_G_average(df1)
+av2 = get_G_average(df2)
+av3 = get_G_average(df3)
+
+#percentage
+pChange = pd.DataFrame()
+for device in liveList:
+    dt1 = av1[av1.device == device]
+    dt2 = av2[av2.device == device]
+    dt3 = av3[av3.device == device]
+    pChange = pChange.append({'device': device, 'PH7_1': dt1.G.mean(), 'PH4': dt2.G.mean(), 'PH7_2': dt3.G.mean()}, ignore_index=True)
+
+pChange['start'] = pChange.PH7_1/pChange.PH7_1
+pChange['7to4'] = pChange.PH4/pChange.PH7_1
+pChange['4to7'] = pChange.PH7_2/pChange.PH7_1
+
+pChange['abs7to4'] = pChange.PH4 - pChange.PH7_1
+
+
+# Figure percentage change
+
+pC = (pChange['7to4']-1)*100
+aC = pChange['abs7to4']
+
+print(av1.G.mean())
+print(av2.G.mean())
+print(av3.G.mean())
+
+#Figures show
+
+plt.style.use('seaborn')
+cm = 1/2.54
+fig1, ((ax1, ax2), (ax3, ax4)) = plt.subplots(ncols=2, nrows=2, figsize=(20*cm, 20*cm))
+ax1.set_title('individual devices')
+ax2.set_title('all devices box plot')
+ax3.set_title('relative change distribution')
+ax4.set_title('absolute change distribution')
+
+#F1
+deviceList = av1.device.unique()
+linestyles = ['-', '--', '-.', ':']
+#plt.figure(num = str(self.name)+'_all')
+#x1.set_xlabel('PH')
+#ax1.set_ylabel('G (S)')
+for device in deviceList:
+    x = ['PH7a', 'PH4', 'PH7b']
+    y = [float(av1.G[av1.device == device]), float(av2.G[av2.device == device]), float(av3.G[av3.device == device])]
+    yerr = [float(av1.sterr[av1.device == device]), float(av2.sterr[av2.device == device]), float(av3.sterr[av3.device == device])]
+    ax1.errorbar(x, y, yerr= yerr)
+    #plt.legend()
+ax1.set_ylabel('G (S)')
+
+#F2
+
+x = ['PH7a', 'PH4', 'PH7b']
+y = [av1.G, av2.G, av3.G]
+ax2.violinplot(y, showmeans=True)
+ax2.set_xlim([0, 4])
+ax2.set_xticks([1,2,3])
+ax2.set_xticklabels(labels=x)
+ax2.set_ylabel('G (S)')
+
+#F3
+
+ax3.hist(pC)
+ax3.set_xlabel('G change from PH7a to PH4 (%)')
+ax3.set_ylabel('number of devices')
+
+#F4
+
+ax4.hist(aC)
+ax4.set_xlabel('G change from PH7a to PH4 (S)')
+ax4.set_ylabel('number of devices')
+
+plt.show()
--- eval.py
+++ eval.py
@@ -6,7 +6,7 @@ class ResultsDF:
 #comment for github
 # Jaccl commits
     def __init__(self, name = 'unnamed'):
-        self.file = easygui.fileopenbox().replace('\\', '/')
+        self.file = easygui.fileopenbox(default= 'G:/Shared drives/Nanoelectronics Team Drive/Data/2021/Marta').replace('\\', '/')
         #self.file = 'C:/Users/marta/OneDrive/Escritorio/Data/20210211/Meas4/MSM01_meas2.csv'
         self.df = pd.read_csv(self.file)
         self.name = name
@@ -70,14 +70,77 @@ class ResultsDF:
 
     def get_live_devices(self, cutoff = 5e-6):
         deviceList = self.df.device.unique()
-        deadList = []
+        liveList = []
         for device in deviceList:
             df1 = self.df[self.df['device'] == device]
             if df1['G'].max() > cutoff:
-                deadList.append(device)
-        return(deadList)
+                liveList.append(device)
+        return(liveList)
+
+###########################################################
+
+def plot_all(df, title = 'title'):
+    deviceList = df.device.unique()
+    linestyles = ['-', '--', '-.', ':']
+    #plt.figure(num = str(self.name)+'_all')
+    plt.title(title)
+    plt.xlabel('time (s)')
+    plt.ylabel('G (S)')
+    for device in deviceList:
+        df1 = df[df['device'] == device]
+        plt.plot(df1['time'], df1['G'], color='C' + str(device), label=device, linestyle=linestyles[device % 4 - 1])
+        plt.legend()
+
+def get_G_average(df):
+    deviceList = df.device.unique()
+    ResultsDF = pd.DataFrame()
+    for device in deviceList:
+        df1 = df[df['device'] == device]
+        #print(df1)
+        ResultsDF = ResultsDF.append({'device': device, 'G': df1.G.mean(), 'sterr': df1.G.sem()}, ignore_index=True)
+    return ResultsDF
+
 
 if __name__ == "__main__":
     S = ResultsDF('sample_A')
     print(S.df.head())
-    S.plot_all()
+
+    #drop dead devices
+    S.df = S.df.loc[(S.df.time > 500)]
+    liveList = S.get_live_devices(cutoff=1e-5)
+    S.df = S.df.loc[(S.df.device.isin(liveList))]
+
+    #split into 3 data frames
+    df1 = S.df.loc[(S.df.time > 500) & (S.df.repeat < 45) & (S.df.device.isin(liveList))]
+    df2 = S.df.loc[(S.df.repeat > 45) & (S.df.repeat < 89) & (S.df.device.isin(liveList))]
+    df3 = S.df.loc[(S.df.repeat > 90) & (S.df.device.isin(liveList))]
+
+    #Get G average for each device
+    av1 = get_G_average(df1)
+    av2 = get_G_average(df2)
+    av3 = get_G_average(df3)
+
+    #percentage
+    pChange = pd.DataFrame()
+    for device in liveList:
+        dt1 = av1[av1.device == device]
+        dt2 = av2[av1.device == device]
+        dt3 = av3[av1.device == device]
+        pChange = pChange.append({'device': device, 'PH7_1': dt1.G.mean(), 'PH4': dt2.G.mean(), 'PH7_2': dt3.G.mean()}, ignore_index=True)
+
+    pChange['start'] = pChange.PH7_1/pChange.PH7_1
+    pChange['7to4'] = pChange.PH4/pChange.PH7_1
+    pChange['4to7'] = pChange.PH7_2/pChange.PH7_1
+
+
+    x = (pChange['7to4']-1)*100
+    plt.hist(x, bins=10)
+
+
+
+
+    print(av1.G.mean())
+    print(av2.G.mean())
+    print(av3.G.mean())
+
+
